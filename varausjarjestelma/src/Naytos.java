7dc1cf7330b612b4d75cb9aa5f0d6cabbf02912b
svuorela <sailvu@utu.fi>
2015-03-23 22:01:08 +0200

    23.3. - 2

---
+++ varausjarjestelma/src/Naytos.java
@@ -0,0 +1,37 @@
+
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
+
+ 
+
+public class Naytos {
+	
+	private boolean[][] istumapaikat;
+	private Calendar ajankohta;
+	private int sali;
+	private Teatteri teatteri;
+	
+	
+	
+	/*
+	//konstruktori
+	public Naytos(int vuosi, int kuukausi, int paiva, boolean[][] istumapaikat, int sali, Teatteri teatteri){
+		ajankohta.set(vuosi, kuukausi, paiva);
+		this.istumapaikat = istumapaikat;
+		this.sali = sali;
+		this.teatteri = teatteri;
+		}
+		*/
+		
+	public Naytos(){
+		
+	}
+		
+		
+		
+		
+	}
+	
+
+
