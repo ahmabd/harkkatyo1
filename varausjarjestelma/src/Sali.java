e51dd41a162e0f37c51de14dadc3f4dc74f289b0
svuorela <sailvu@utu.fi>
2015-03-23 21:59:11 +0200

    commit 23.3. Sali - melkein valmis

--- varausjarjestelma/src/Sali.java
+++ varausjarjestelma/src/Sali.java
@@ -7,25 +7,185 @@ import java.util.ArrayList;
 public class Sali {
 	//testaukseen
 	private boolean verbose = false;
+	private boolean tulostapaikat = false;
 
 	private int salinumero;
-	private boolean[][] istumapaikat;
-	private Teatteri teatteri;
-	private Naytos[] naytokset;
+	//null -> vapaa, varausnumero -> varattu
+	private String[][] istumapaikat;
+	private String  teatterinNimi;
+	private String teatterinPaikkakunta;
+	private ArrayList<Naytos>[] naytokset;
+	//onko katsomon koko määrittämättä
+	boolean kesken;
 
 	//konstruktori
-	public Sali(int salinumero){
+	public Sali(int salinumero, int rivit, int sarakkeet, String teatterinNimi, String teatterinPaikkakunta){
 		if(verbose){System.out.println("Luokka: Sali : konstruktori");}
-		this.salinumero=salinumero;
 
+		this.salinumero=salinumero;
+		this.teatterinNimi = teatterinNimi;
+		this.teatterinPaikkakunta = teatterinPaikkakunta;
+		istumapaikat = new String[rivit][sarakkeet];
+
+		//tarkistaa onko salin muokkaus vielä kesken
+		if(rivit == 1 && sarakkeet == 1){
+			kesken = true;
+		}else{
+			kesken = false;
+		}
+		alustaPaikat();
+		alustaNaytokset();
+			
 	}
+
 
 	//getterit ja setterit
 	//------------------------------------------------------------
 
+	/**
+	 * palauttaa salinumeron
+	 * @return : Int
+	 */
 	public int annaSalinumero(){
 		if(verbose){System.out.println("Luokka: Sali : annaSalinumero()");}
-
 		return salinumero;
+	}
+	/**
+	 * palauttaa teatterin nimen
+	 * @return : String
+	 */
+	public String annaTeatterinNimi(){
+		if(verbose){System.out.println("Luokka: Sali : annaTeatterinNimi()");}
+		return teatterinNimi;
+	}
+
+	/**
+	 * palauttaa teatterin paikkakunnan
+	 * @return : String
+	 */
+	public String annaTeatterinPaikkakunta(){
+		if(verbose){System.out.println("Luokka: Sali : annaTeatterinPaikkakunta()");}
+		return teatterinPaikkakunta;
+	}
+
+	/**
+	 * palauttaa teatterin istumapaikat
+	 * @return : String[][]
+	 */
+	public String[][] annaIstumapaikat(){
+		if(verbose){System.out.println("Luokka: Sali : annaIstumapaikat()");}
+		return istumapaikat;
+	}
+
+	/** TODO testaa!
+	 * palauttaa kaikki salin näytökset
+	 * @return ArrayList<Naytos>[]
+	 */
+	public ArrayList<Naytos>[] annaKaikkiNaytokset(){
+		if(verbose){System.out.println("Luokka: Sali : annaIstumapaikat()");}
+		return naytokset;
+	}
+	
+	/** TODO testaa! 
+	 * palauttaa tietyn kellonajan näyökset, 1= klo 10, 2= klo 13, 3= klo 16, 4= klo 19, 5= klo 22
+	 * @param kellonaika
+	 * @return ArrayList<Naytos> tai null mikäli kellonaika ei ole 1-5
+	 */
+	public ArrayList<Naytos> annaNaytokset(int kellonaika){
+		if(kellonaika > 0 && kellonaika <6){
+			return naytokset[kellonaika];
+		}else{
+			return null;
+		}
+	}
+
+	/**
+	 * Asettaa salin koon halutun kokoiseksi istumapaikkojen suhteen
+	 * @param rivit
+	 * @param sarakkeet
+	 */
+	public void asetaSalinKoko(int rivit, int sarakkeet){
+		if(verbose){System.out.println("Luokka: Sali : asetaSalinKoko()");}
+		istumapaikat = new String[rivit][sarakkeet];
+		alustaPaikat();
+	}
+
+
+
+	//------------------------------------------------------------
+
+	/**
+	 * TODO tee!
+	 */
+	public void lisaaNaytos(){
+		
+	}
+	/**
+	 * Alustaa näytöspaikat ArrayList<Naytos> olioilla
+	 */
+	private void alustaNaytokset(){
+		naytokset = new ArrayList[5];
+		for(int i=0; i<naytokset.length; i++){
+			naytokset[i] = new ArrayList<Naytos>();
+		}
+	}
+	
+	public boolean onkoKesken(){
+		if(verbose){System.out.println("Luokka: Sali : onkoKesken()");}
+		return kesken;
+	}
+	/**
+	 * Alustaa istumapaikat arvoilla null (=vapaa)
+	 * @param istumapaikat
+	 */
+	private void alustaPaikat(){
+		if(verbose){System.out.println("Luokka: Sali : alustaPaikat()");}
+
+		int rivi = istumapaikat.length;
+		int sarake = istumapaikat[0].length;		
+
+		for(int i=0; i<rivi; i++){
+			for(int j=0; j<sarake; j++){
+				istumapaikat[i][j] = null;				
+			}		
+		}
+
+		if(tulostapaikat){tulostaPaikat();}
+	}
+
+	/** TESTAUKSEEN
+	 * Tulostaa istumapaikat ja arvot
+	 * @param istumapaikat
+	 */
+	public void tulostaPaikat(){
+		if(verbose){System.out.println("Luokka: Sali : tulostaPaikat()");}
+
+		int rivi = istumapaikat.length;
+		int sarake = istumapaikat[0].length;
+		for(int j=0; j<sarake; j++){
+			System.out.print("-------");
+		}
+		System.out.println("");
+		for(int i=0; i<rivi; i++){
+
+			System.out.print("|");
+			for(int j=0; j<sarake; j++){
+				System.out.print(" " +istumapaikat[i][j]+" |");
+			}
+			System.out.println("");
+			for(int j=0; j<sarake; j++){
+				System.out.print("-------");
+			}
+			System.out.println("");
+		}
+
+	}
+	
+	public String toString(){
+		if(verbose){System.out.println("Luokka: Sali : toString()");}
+		return("Salinumero: "+salinumero+
+				", Teatterin nimi: "+teatterinNimi+
+				", Teatterin paikkakunta: "+teatterinPaikkakunta+
+				", salin koko: "+istumapaikat.length+ " x "+istumapaikat[0].length);
 	}
 }
--- varausjarjestelma/src/Testausluokka.java
+++ varausjarjestelma/src/Testausluokka.java
@@ -3,7 +3,10 @@ import java.util.Calendar;
 import java.util.GregorianCalendar;
 import java.util.Scanner;
 
-
+/*
+ * Lis‰‰ informaatiota ohjelman toiminnasta saa kun vaihtaa luokkien verbose-muuttujat arvoksi true!
+ * lis‰ksi salipaikat tulostetaan aina jos Sali-luokan tulostapaikat-muuttujan arvo on true.
+ */
 public class Testausluokka {
 	
 	//nopeuttamaan kirjoittamista
@@ -18,7 +21,7 @@ public class Testausluokka {
 		//Elokuva-testi
 		//konstruktori
 		print("Luodaan Elokuva-olio 'elokuva'. Nimi = Hessu Hermanni. Kesto = 123 min\n ");		
-		Elokuva elokuva = new Elokuva("hessu",123);
+		Elokuva elokuva = new Elokuva("Hessu Hermanni",123);
 		print("Elokuva luotu");
 		//getterit
 		print("Nimi : "+elokuva.annaNimi()+" ja kesto: "+elokuva.annaKesto());
@@ -44,31 +47,85 @@ public class Testausluokka {
 			System.out.println(salit.get(i).annaSalinumero());
 		}
 		//setterit
-		print("muokataan salin nimeksi: Ikituuri, paikkakunnaksi Nummi sek‰ salim‰‰r‰ksi 3");
+		print("muokataan teatterin nimeksi: Ikituuri, paikkakunnaksi Nummi sek‰ salim‰‰r‰ksi 3");
 		teatteri1.asetaNimi("Ikituuri");
 		teatteri1.asetaPaikkakunta("Nummi");
 		teatteri1.asetaSalimaara(3);
 		print("uudet tiedot: " +teatteri1.toString());
 		//lis‰t‰‰n sali
-		print("lis‰t‰‰n uusi sali muiden lis‰ksi");
-		teatteri1.lisaaSali();		
+		print("lis‰t‰‰n uusi sali muiden lis‰ksi. Katsomo 10 x 10");
+		teatteri1.lisaaSali(10,10);		
 		print("teatterin tiedot: "+teatteri1.toString());
 		//poistetaan sali
 		print("poistetaan sali numero 2");
 		teatteri1.poistaSali(2);
 		salit = teatteri1.annaSalit();		
 		print("teatterin tiedot: "+teatteri1.toString());
-		print("lis‰t‰‰n sali id:ll‰ 3");
-		if(teatteri1.lisaaSali(3) == false){
+		print("lis‰t‰‰n sali id:ll‰ 3, Katsomo 10x10");
+		if(teatteri1.lisaaSali(3, 10, 10) == false){
 			print("Ei voitu lis‰t‰, sali jo olemassa!");
 		}
 		print("teatterin tiedot: "+teatteri1.toString());
-		print("lis‰t‰‰n sali id:ll‰ 2");
-		if(teatteri1.lisaaSali(2) == false){
+		print("lis‰t‰‰n sali id:ll‰ 2, Katsomo 10 x 20");
+		if(teatteri1.lisaaSali(2, 10, 20) == false){
 			print("Ei voitu lis‰t‰, sali jo olemassa!");
 		}else{print("Sali lis‰tty!");}
 		salit = teatteri1.annaSalit();		
 		print("teatterin tiedot: "+teatteri1.toString());
+		
+		//--------------------------------------------------------------------------------------------
+		
+		//Sali-testi
+		//Konstruktori
+		print("\nLuodaan uusi sali teatteri1:lle. katsomo 7 x 10");
+		teatteri1.lisaaSali(7, 10);
+		System.out.println("Uuden salin numero: "+teatteri1.annaSali(teatteri1.annaSalit().size()).annaSalinumero());
+		//getterit
+		print("Sali on teatterissa: "+ teatteri1.annaSali(5).annaTeatterinNimi()+", paikkakunnalla: "+teatteri1.annaSali(5).annaTeatterinPaikkakunta());
+		print("Salin istumapaikat: ");
+		//paikkatulostus
+		teatteri1.annaSali(5).tulostaPaikat();
+		print("teatteri1:n kaikkien salien paikat:");
+		for(int i=0; i<teatteri1.annaSalit().size(); i++){
+			System.out.println(teatteri1.annaSali(i+1).annaSalinumero()+": ");
+			teatteri1.annaSali(i+1).tulostaPaikat();
+		}
+		//onkoKesken()
+		print("valmiita saleja: ");
+		for(int i=0; i<teatteri1.annaSalit().size();i++){
+			if(teatteri1.annaSali(i+1).onkoKesken()){
+			}else{
+				System.out.println(teatteri1.annaSali(i+1).annaSalinumero());
+			}
+		}
+		print("keskener‰isi‰ saleja: ");
+		ArrayList<Sali> kesken = new ArrayList<Sali>();
+		for(int i=0; i<teatteri1.annaSalit().size();i++){
+			if(teatteri1.annaSali(i+1).onkoKesken()){
+				System.out.println(teatteri1.annaSali(i+1).annaSalinumero());
+				kesken.add(teatteri1.annaSali(i+1));
+			}
+		}
+		//asetaSalinKoko
+		print("asetetaan keskener‰isiin saleihin koot. ensimm‰inen 5x5, seuraavat aina yhden isompia");
+		for(int i=0; i<kesken.size();i++){
+			System.out.println("Sali numero: "+kesken.get(i).annaSalinumero());
+			kesken.get(i).asetaSalinKoko(5+i, 5+i);
+			kesken.get(i).tulostaPaikat();			
+		}		
+		print("teatteri1:n kaikkien salien paikat:");
+		for(int i=0; i<teatteri1.annaSalit().size(); i++){
+			System.out.println(teatteri1.annaSali(i+1).annaSalinumero()+": ");
+			teatteri1.annaSali(i+1).tulostaPaikat();
+		}
+		//toString
+		print("Teatteri1:n ja sen salien tiedot:");
+		print(teatteri1.toString());
+		for(int i=0; i<teatteri1.annaSalit().size();i++){
+			print(teatteri1.annaSali(i+1).toString());
+		}
+	
+		
 		
 		//N‰ytˆs-testi
 		//konstruktori
